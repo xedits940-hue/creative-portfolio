3bbd0c312d0d233bb98aa0a4ed13b4659b3f460e
xedits940-hue <xedits940@gmail.com>
2026-08-13 07:22:48 +0530

    Update manifest.ts

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -11,9 +11,9 @@ export default function manifest(): MetadataRoute.Manifest {
     theme_color: '#000000',
     icons: [
       {
-        src: '/md-red-logo.svg',
+        src: '/logo.png',
         sizes: 'any',
-        type: 'image/svg+xml',
+        type: 'image/png',
       },
     ],
   }
