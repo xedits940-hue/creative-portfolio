bcddcf3de78736ffe5e4f5538fce15272bda2cca
xedits940-hue <xedits940@gmail.com>
2026-08-13 16:15:01 +0530

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -98,16 +98,13 @@ export const metadata: Metadata = {
   },
   icons: {
     icon: [
-      { url: "/icon" },
-      { url: "/logo.png", type: "image/png", sizes: "32x32" },
       { url: "/logo.png", type: "image/png", sizes: "16x16" },
+      { url: "/logo.png", type: "image/png", sizes: "32x32" },
       { url: "/logo.png", type: "image/png", sizes: "192x192" },
       { url: "/logo.png", type: "image/png", sizes: "512x512" },
     ],
     shortcut: "/logo.png",
-    apple: [
-      { url: "/logo.png", sizes: "180x180", type: "image/png" },
-    ],
+    apple: [{ url: "/logo.png", sizes: "180x180", type: "image/png" }],
   },
   manifest: "/manifest.webmanifest",
   verification: {
