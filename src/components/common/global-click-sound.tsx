05894d4de579d7927fd8b411bc1a6cc34fa1a920
xedits940-hue <xedits940@gmail.com>
2026-08-17 14:35:07 +0530

    Update global-click-sound.tsx

--- src/components/common/global-click-sound.tsx
+++ src/components/common/global-click-sound.tsx
@@ -7,18 +7,16 @@ export default function GlobalClickSound() {
   useEffect(() => {
     const handleClick = (e: MouseEvent) => {
       const target = e.target as HTMLElement;
+
       // Elements marked with data-sound-exclude will NOT trigger this
-      // universal click sound (used for buttons that already have their
-      // own dedicated sound, like theme-toggle, hamburger menu, etc.)
+      // universal click sound.
       if (target.closest("[data-sound-exclude]")) return;
 
-      playSound(
-        "https://www.myinstants.com/en/instant/nintendo-switch-click-69023/?utm_source=copy&utm_medium=share",
-        0.35
-      );
+      playSound("/switch-sound.mp3", 0.35);
     };
 
     document.addEventListener("click", handleClick);
+
     return () => document.removeEventListener("click", handleClick);
   }, []);
 
